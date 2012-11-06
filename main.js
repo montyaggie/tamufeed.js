4cc0783faeeb7ae18a8a4dc5a986386dab728802
Monty Dickerson <monty@tamu.edu>
2012-11-06 00:39:54 -0600

    minor, cosmetic

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-//// Fallback if an AMD asynchronous module definition loader not in use ////
-  if (typeof define !== 'function' || !define.amd)
-    var define = function (id,args,func) { 
-      this[id] = func; this[id].apply(this,args); 
-  }//define
-
 define('main', ['jquery','tamufeed'], function($,tamufeed) {
-/******************************************************************************/
+/************************************************************/
 
 $(function() { tamufeed.init(); });
 
-/******************************************************************************/
+/************************************************************/
 });
